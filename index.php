1a0e3de98373c5edf517184e7e53ae77e9087e0b
P3PRLX-stack <85352689+P3PRLX-stack@users.noreply.github.com>
2022-05-03 12:09:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>HaCkeD By Dead Exploiter!</title>
+</head>
+
+<body bgcolor=black oncontextmenu="return false;" onkeydown="return false;" onload="teclear();" onmousedown="return false;">
+
+
+<center><img src="https://i.top4top.io/p_2314gi62f1.jpg" width="700" height "50" border="0" /></div>
+
+
+<center><img border="0" height="500" src="https://i.top4top.io/p_2314gi62f1.jpg" width="700" /></td>
+<cente>
+
+<p align="center" dir="ltr"><font size="5" color="#58FAF4"> Don't worry!! We won't do anything bad. We are just showing you guys that your system is weak so make it secure. We can help you in this, you can contact us and we will tell you what the vulnerability was and how could you fix it. We don't have any bad intension. If we had bad intention, we would have completly destroyed your whole system but we haven't done anything expect defacing and redirecting the site. We know you will fix this within 1 or 2 days. If we had just reported the bug, you would not have ben serious about this so this hack is just to increase your efficiency. We want safe cyber security in INDIA so that foreigners couldn't hack into our systems and steal our data and money. Ofcourse we love security that's why we are #Instead of searching for us, please secure your systems. We are always active to make INDIA's online time more secure. Lets secure digital INDIA together. We are looking for your support. Decrease dependency with NEPAL as much as possible. Then only Greater INDIA is possible, which is our main objective
+
+<p align="center" dir="ltr"><font size="5" color="#58FAF4"> We Are INDIA, We Love Peace
+
+<marquee behavior="alternate">
+<img src="https://i.top4top.io/p_2314gi62f1.jpg" width="700" height="500" alt="Dead Exploiter" />
+</marquee>
+
+<footer id="det" style="position:fixed; left:0px; right:0px; bottom:0px; background:rgb(0,0,0); text-align:center; border-top: 1px solid #58FAF4; border-bottom: 1px solid #58FAF4">
+                        <p>
+                                          </embed><footer id="det" style="position:fixed; left:0px; right:0px; bottom:0px; background:rgb(0,0,0); text-align:center; border-top: 1px solid #58FAF4; border-bottom: 1px solid #58FAF4"><font color=#ff0000 size=2 face="Tahoma"><font color="58FAF4"><b>Official Members : </b></font><marquee scrollamount="5" scrolldelay="50" width="80%"><b>Indian Hackers
+</b></marquee> 
+                
+
+                </footer>
+        </body>
+</html>
